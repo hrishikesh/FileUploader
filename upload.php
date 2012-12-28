628c552a7aecb74d54f18832b499b58d54570db7
Hrishikesh <hrishikesh@weboniselab.com>
2012-12-28 13:49:13 +0530

    Removed Commented Code

--- upload.php
+++ upload.php
@@ -12,24 +12,6 @@ error_reporting(E_ALL);
 $fileUploader = new FileUploader($_FILES['mf_file_aupload']);
 $fileUploader->uploadFile();
 
-//$fileUploader->getPdoAvailableDrivers();
-
-/*ini_set("memory_limit", "1024M" );
-
-define("UPLOAD_DIR", dirname(__FILE__) . '/uploads/');
-
-
-$file = $_FILES['mf_file_aupload'];
-
-$filePath = UPLOAD_DIR.$file['name'];
-
-
-
-
-if(move_uploaded_file($file['tmp_name'], $filePath)) {
-    echo json_encode(array('filename'=>$file['name'], 'filePath'=>$filePath,'message'=>'Upload Successful', 'size'=>filesize($filePath)));die;
-}*/
-
 
 
 
